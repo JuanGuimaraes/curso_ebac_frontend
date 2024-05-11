a8b454d4d13fe40386f1540a493754b248b21b09
JuanGuimaraes <juandasmerces@outlook.com>
2024-05-11 13:29:04 -0300

    corrigindo o codigo, criando o array de alunos e filtrando os aprovados

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-var _this = void 0;
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
@@ -12,19 +11,50 @@ var Aluno = /*#__PURE__*/_createClass(function Aluno(nome, nota) {
   this.nome = nome;
   this.nota = nota;
 });
-var aluno1 = new Aluno("Jose", 10);
-var aluno2 = new Aluno("Maria", 9);
-var aluno3 = new Aluno("Aline", 6);
-var aluno4 = new Aluno("Marcos", 3);
-var media = function media(nome, nota) {
-  var notaMinima = 6;
-  _this.nome = nome;
-  _this.nota = nota;
-  if (notaMinima <= nota) {
-    return "Ol\xE1 ".concat(nome, ", sua nota ").concat(nota, " \xE9 otima, aprovado");
-  } else {
-    return "Ol\xE1 ".concat(nome, ", sua ").concat(nota, " est\xE1 baixa, reprovado");
-  }
-};
-var exibeMedia = media;
-console.log(media(aluno1.nome, aluno1.nota));
+var alunos = [{
+  nome: 'Jose',
+  nota: 10
+}, {
+  nome: 'Maria',
+  nota: 3
+}, {
+  nome: 'Aline',
+  nota: 6
+}, {
+  nome: 'Marcos',
+  nota: 9
+}, {
+  nome: 'Julia',
+  nota: 5
+}, {
+  nome: 'Pedro',
+  nota: 1
+}, {
+  nome: 'João',
+  nota: 10
+}];
+var alunosAprovados = alunos.filter(function (aluno) {
+  return aluno.nota >= 6;
+});
+console.log(alunosAprovados);
+
+// let aluno1 = new Aluno("Jose", 10)
+// let aluno2 = new Aluno("Maria", 9)
+// let aluno3 = new Aluno("Aline", 6)
+// let aluno4 = new Aluno("Marcos",3)
+
+//     const media = (nome, nota) => {
+//                 let notaMinima = 6;
+//                 this.nome = nome
+//                 this.nota = nota
+
+//                 if (notaMinima <= nota) {
+//                     return (`Olá ${nome}, sua nota ${nota} é otima, aprovado`);
+//                 } else{
+//                     return (`Olá ${nome}, sua ${nota} está baixa, reprovado`);
+//                 }
+//             };
+
+// const exibeMedia = media;
+
+// console.log(media(aluno1.nome, aluno1.nota));
--- src/main.js
+++ src/main.js
@@ -5,29 +5,42 @@ class Aluno {
     }
 }
 
+const alunos = [
+    {nome: 'Jose', nota: 10},
+    {nome: 'Maria', nota: 3},
+    {nome: 'Aline', nota: 6},
+    {nome: 'Marcos', nota: 9},
+    {nome: 'Julia', nota: 5},
+    {nome: 'Pedro', nota: 1},
+    {nome: 'João', nota: 10},
+]
 
-    let aluno1 = new Aluno("Jose", 10)
-    let aluno2 = new Aluno("Maria", 9)
-    let aluno3 = new Aluno("Aline", 6)
-    let aluno4 = new Aluno("Marcos",3)
+const alunosAprovados = alunos.filter(aluno => aluno.nota >= 6);
+
+console.log(alunosAprovados);
+
+    // let aluno1 = new Aluno("Jose", 10)
+    // let aluno2 = new Aluno("Maria", 9)
+    // let aluno3 = new Aluno("Aline", 6)
+    // let aluno4 = new Aluno("Marcos",3)
 
 
-    const media = (nome, nota) => {
-                let notaMinima = 6;
-                this.nome = nome
-                this.nota = nota
+//     const media = (nome, nota) => {
+//                 let notaMinima = 6;
+//                 this.nome = nome
+//                 this.nota = nota
 
-                if (notaMinima <= nota) {
-                    return (`Olá ${nome}, sua nota ${nota} é otima, aprovado`);
-                } else{
-                    return (`Olá ${nome}, sua ${nota} está baixa, reprovado`);
-                }
-            };
+//                 if (notaMinima <= nota) {
+//                     return (`Olá ${nome}, sua nota ${nota} é otima, aprovado`);
+//                 } else{
+//                     return (`Olá ${nome}, sua ${nota} está baixa, reprovado`);
+//                 }
+//             };
 
 
-const exibeMedia = media;
+// const exibeMedia = media;
 
-console.log(media(aluno1.nome, aluno1.nota));
+// console.log(media(aluno1.nome, aluno1.nota));
 
 
 
